a9fce6e216dcc274830ad2ee38ec432103a4d75e
AndrewNyirenda <andrewkaynyirenda@gmail.com>
2026-02-01 03:36:34 +0200

    Refactored Landing Page

--- src/components/Landing_page.jsx
+++ src/components/Landing_page.jsx
@@ -1,49 +1,22 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import Header from "./Header";
 import "../styles/landing_page.css";
 import Services from './landing-page/Services';
 import EduResources from './landing-page/EduResources';
 import Committment from './landing-page/Committment';
 import Footer from "./Footer";
-import { SearchContext } from '../components/SearchContext';
 import HeroSection from './landing-page/HeroSection';
 
 const LandingPage = () => {
-  const { query, results, setResults, showResults, setShowResults } = useContext(SearchContext);
-
-  useEffect(() => {
-    if (!query.trim()) {
-      setResults([]);
-      setShowResults(false);
-      return;
-    }
-
-    const filtered = allResources.filter(
-      (res) =>
-        res.title.toLowerCase().includes(query.toLowerCase()) ||
-        res.category.toLowerCase().includes(query.toLowerCase())
-    );
-    setResults(filtered);
-    setShowResults(true);
-  }, [query]);
-
   return (
     <div className="LandingPageWrapper">
       <Header />
-
-      {showResults ? (
-        <div className="search-results">
-          
-        </div>
-      ) : (
-        <>
-          <HeroSection />
-          <EduResources />
-          <Services />
-          <Committment />
-        </>
-      )}
-
+      
+      <HeroSection />
+      <EduResources />
+      <Services />
+      <Committment />
+      
       <Footer />
     </div>
   );
